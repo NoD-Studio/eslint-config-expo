d95d557ce496a9afbf2a60c2b1b9a4e647aacd07
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2025-02-03 10:21:42 +0100

    refacto: Implement ESLint config React native

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-# eslint-config-nodstudio-reactnative
+# eslint-config-expo
+
+## Install
+
+1. Install Eslint on your projet
+
+```shell
+yarn add --dev eslint @eslint/js
+```
+
+2. Install config
+
+```shell
+yarn add --dev @nodstudio/eslint-config-expo
+```
+
+3. Add config to yours
+
+```js
+import nodStudioConfigExpo from "@nodstudio/eslint-config-expo";
+
+export default [
+    // ...
+    ...nodStudioConfigExpo
+];
+```
+
+## Usage
+
+### Imports
+
+The goal is to force a file structure logic where low-levels components can't import high-levels components.
+
+| folder                                                                      | can't import                                                                                                                                                                                   |
+|-----------------------------------------------------------------------------|------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| @design/\*\*/\*<br>@/design/\*\*/\*<br>design/\*\*/\*<br>src/design/\*\*/\* | @containers/\*\*/\*<br>@/containers/\*\*/\*<br>containers/\*\*/\*<br>src/containers/\*\*/\*<br><br>@components/\*\*/\*<br>@/components/\*\*/\*<br>components/\*\*/\*<br>src/components/\*\*/\* |
+| @components/\*\*/\*<br>@/components/\*\*/\*<br>components/\*\*/\*<br>src/components/\*\*/\* | @containers/\*\*/\*<br>@/containers/\*\*/\*<br>containers/\*\*/\*<br>src/containers/\*\*/\*                                                                                                    |                                                                                                     |   |   |   |
+
+### Examples
+
+With new expo file structure :
+.  
+├── app  
+├── assets  
+├── components (can't import from ./containers)  
+├── containers  
+├── db  
+├── design (can't import from ./components or ./containers)  
+├── doc  
+├── enums  
+├── game  
+├── models  
+├── scripts  
+└── utils  
+
+With old expo file structure :  
+.  
+├── assets  
+├── doc  
+├── scripts  
+└── src  
+&nbsp;&nbsp;&nbsp;&nbsp;├── app  
+&nbsp;&nbsp;&nbsp;&nbsp;├── components (can't import from src/containers)  
+&nbsp;&nbsp;&nbsp;&nbsp;├── containers  
+&nbsp;&nbsp;&nbsp;&nbsp;├── db  
+&nbsp;&nbsp;&nbsp;&nbsp;├── design (can't import from src/components or src/containers)  
+&nbsp;&nbsp;&nbsp;&nbsp;├── enums  
+&nbsp;&nbsp;&nbsp;&nbsp;├── game  
+&nbsp;&nbsp;&nbsp;&nbsp;├── models  
+&nbsp;&nbsp;&nbsp;&nbsp;└── utils  
--- index.js
+++ index.js
@@ -1,139 +1,60 @@
-// @ts-nocheck
-import { FlatCompat } from "@eslint/eslintrc";
-import { default as eslint } from "@eslint/js";
-import typescriptParser from "@typescript-eslint/parser";
-// @ts-expect-error ImportPlugin is not typed apparently
-import importPlugin from "eslint-plugin-import";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import reactPlugin from "eslint-plugin-react";
-import reactNative from "eslint-plugin-react-native";
-import reactRecommended from "eslint-plugin-react/configs/recommended.js";
-import tailwind from "eslint-plugin-tailwindcss";
-import globals from "globals";
-import path from "path";
-import tseslint from "typescript-eslint";
-import { fileURLToPath } from "url";
-
-// mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+// @ts-check
+import nodStudioConfigReactNative from "@nodstudio/eslint-config-react-native";
 
 export default [
-  ...tseslint.config(
-      eslint.configs.recommended,
-      tseslint.configs.recommended,
-    // @ts-expect-error Confirmed type error when using React ESlint with Typescript ESLint https://github.com/typescript-eslint/typescript-eslint/issues/8522
-    reactRecommended,
-    reactPlugin.configs.flat?.["jsx-runtime"],
-    {
-      files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
-      settings: {
-        react: {
-          version: "detect",
+  ...nodStudioConfigReactNative,
+  {
+    files: ["@design/**/*", "@/design/**/*", "design/**/*", "src/design/**/*"],
+    rules: {
+      "@typescript-eslint/no-restricted-imports": [
+        "error",
+        {
+          patterns: [
+            {
+              group: [
+                "@components/**/*",
+                "@/components/**/*",
+                "components/**/*",
+                "src/components/**/*",
+              ],
+              message: "Design can't import from components.",
+            },
+            {
+              group: [
+                "@containers/**/*",
+                "@/containers/**/*",
+                "containers/**/*",
+                "src/containers/**/*",
+              ],
+              message: "Design can't import from containers.",
+            },
+          ],
         },
-      },
-      languageOptions: {
-        parser: typescriptParser,
-        globals: {
-          ...globals.serviceworker,
-          ...globals.browser,
-        },
-      },
-    },
-    {
-      rules: {
-        "@typescript-eslint/no-require-imports": [
-          "off",
-          {
-            allow: ["^assets/.*$", "react-native-google-mobile-ads"],
-          },
-        ],
-        "@typescript-eslint/no-duplicate-enum-values": "off",
-      },
-    },
-    {
-      files: ["design/**/*"],
-      rules: {
-        "@typescript-eslint/no-restricted-imports": [
-          "error",
-          {
-            patterns: [
-              {
-                group: ["@/components/*", "components/*"],
-                message: "Design can't import from components.",
-              },
-              {
-                group: ["@/containers/*", "containers/*"],
-                message: "Design can't import from containers.",
-              },
-            ],
-          },
-        ],
-      },
-    },
-    {
-      files: ["components/**/*"],
-      rules: {
-        "@typescript-eslint/no-restricted-imports": [
-          "error",
-          {
-            patterns: [
-              {
-                group: ["@/containers/*", "containers/*"],
-                message: "Components can't import from containers.",
-              },
-            ],
-          },
-        ],
-      },
-    },
-    {
-      plugins: { react: reactPlugin },
-      rules: {
-        "react/jsx-fragments": ["error", "syntax"],
-      },
-    },
-    {
-      plugins: {
-        "react-native": reactNative,
-      },
-
-      rules: {
-        "react-native/no-raw-text": 2,
-      },
-    },
-    ...compat.extends("plugin:react-hooks/recommended"),
-    eslintPluginPrettierRecommended,
-    ...tailwind.configs["flat/recommended"],
-    {
-      plugins: {
-        "import-plugin": importPlugin,
-      },
-      rules: {
-        "import-plugin/newline-after-import": ["error", { count: 1 }],
-        // TODO : Can't use this rule right now because it's bugged
-        // Will activate it later when it's resolved
-        // Source : https://github.com/import-js/eslint-plugin-import/issues/3079
-        // "import-plugin/no-unused-modules": [
-        //   "error",
-        //   {
-        //     unusedExports: true,
-        //   },
-        // ],
-      },
-    },
-    {
-      ignores: [
-        "*.config.js",
-        "*.config.mjs",
-        "*.config.ts",
-        "*-env.d.ts",
-        "scripts/reset-project.js",
       ],
     },
-  ),
+  },
+  {
+    files: ["@components/**/*", "@/components/**/*", "components/**/*", "src/components/**/*"],
+    rules: {
+      "@typescript-eslint/no-restricted-imports": [
+        "error",
+        {
+          patterns: [
+            {
+              group: [
+                "@containers/**/*",
+                "@/containers/**/*",
+                "containers/**/*",
+                "src/containers/**/*",
+              ],
+              message: "Components can't import from containers.",
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    ignores: ["*-env.d.ts", "scripts/reset-project.js"],
+  },
 ];
